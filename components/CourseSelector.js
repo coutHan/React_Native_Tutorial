c0adb066a7635701c9d7c06291371e6ae98ae2c8
coutHan <hancao2021@u.northwestern.edu>
2020-12-05 01:05:27 -0800

    course detail

--- components/CourseSelector.js
+++ components/CourseSelector.js
@@ -1,9 +1,10 @@
+
 import React, {useState} from 'react';
 import { StyleSheet, View, Text} from 'react-native';
 import Course from './Course';
-import { hasConflict } from '../Utils/course';
+import { hasConflict } from '../utils/course';
 
-const CourseSelector = ({courses}) => {
+const CourseSelector = ({courses, view}) => {
     const [selected, setSelected] = useState([]);
     const toggle = course => setSelected(selected => (
         selected.includes(course) ? selected.filter(x => x !== course) : [...selected, course]
@@ -16,6 +17,7 @@ const CourseSelector = ({courses}) => {
                         isDisabled = {hasConflict(course, selected)}
                         isSelected = {selected.includes(course)} 
                         select = {toggle}
+                        view = {view}
                         />))
             };
         </Text>
