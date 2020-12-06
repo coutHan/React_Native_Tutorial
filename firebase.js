30270871148ee0dfe5bd43f2dd1dbfded8d6aa8d
coutHan <hancao2021@u.northwestern.edu>
2020-12-05 22:00:27 -0800

    firebase

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+
+import "firebase/database";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCZzW_zMeSYO5vfB1dKLq3bCQCAF5D9avU",
+    authDomain: "tutorial-5e34f.firebaseapp.com",
+    databaseURL: "https://tutorial-5e34f.firebaseio.com",
+    projectId: "tutorial-5e34f",
+    storageBucket: "tutorial-5e34f.appspot.com",
+    messagingSenderId: "734009038382",
+    appId: "1:734009038382:web:52c4d541308fcdfd08b1bf",
+    measurementId: "G-KDS6PQ1Q24"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export { firebase };
